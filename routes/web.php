220a2162ae5f739aecf3a8ff042f86d8776a23d5
WalterCorsini <corsini.walter@gmail.com>
2024-06-06 17:12:21 +0200

    fix web.php home

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home-page', function () {
+Route::get('/', function () {
     $data_home = [
         'companies' => ["Goldman Sachs","Oaktree","Elioot","Boolean",]
     ];
